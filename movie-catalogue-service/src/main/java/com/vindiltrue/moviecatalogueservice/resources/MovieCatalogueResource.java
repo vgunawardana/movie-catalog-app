b12df1959f6c39827e0d13353717a3ceccb006a0
vgunawardana <vindi121@gmail.com>
2019-10-15 15:57:30 +0530

    Call Ratings API using userID

--- movie-catalogue-service/src/main/java/com/vindiltrue/moviecatalogueservice/resources/MovieCatalogueResource.java
+++ movie-catalogue-service/src/main/java/com/vindiltrue/moviecatalogueservice/resources/MovieCatalogueResource.java
@@ -18,50 +18,44 @@ import com.vindiltrue.moviecatalogueservice.models.Rating;
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogueResource {
-	
-	//@Autowired
-	//private RestTemplate restTemplate;
-	
-	@Autowired
-	private WebClient.Builder webClientBuilder;
-	
-	@RequestMapping("/{userId}")
-	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-		
-		//RestTemplate restTemplate = new RestTemplate();
-		//restTemplate.getForObject("http://localhost:8082/movie/foo", Movie.class);
-		
-		
-		//Create a builder
-		//WebClient.Builder builder = WebClient.builder();
-		
-		
-		List<Rating> ratings = Arrays.asList(
 
-				new Rating("0001",4),
-				new Rating("0002",3)
-		);
+	@Autowired
+	private RestTemplate restTemplate;
+
+	// @Autowired
+	// private WebClient.Builder webClientBuilder;
+
+	@RequestMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+		// RestTemplate restTemplate = new RestTemplate();
+		// restTemplate.getForObject("http://localhost:8082/movie/foo", Movie.class);
+
+		// Create a builder
+		// WebClient.Builder builder = WebClient.builder();
+
 		
+		  List<Rating> ratings = Arrays.asList(
+		  
+		  new Rating("0001",4), new Rating("0002",3) );
+		 
+
 		return ratings.stream().map(rating -> {
-				//Movie movie = restTemplate.getForObject("http://localhost:8082/movie/"+rating.getMovieId(), Movie.class);
-			
-				Movie movie = webClientBuilder.build()
-					.get()
-					.uri("http://localhost:8082/movie/"+rating.getMovieId())
-					.retrieve()
-					.bodyToMono(Movie.class)
-					.block();
-				
-				return new CatalogItem(movie.getName(), "Test", rating.getRatings());
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movie/" + rating.getMovieId(), Movie.class);
+
+			/*
+			 * Movie movie = webClientBuilder.build() .get()
+			 * .uri("http://localhost:8082/movie/"+rating.getMovieId()) .retrieve()
+			 * .bodyToMono(Movie.class) .block();
+			 */
+			return new CatalogItem(movie.getName(), "Test", rating.getRatings());
 		}).collect(Collectors.toList());
-		
-		
+
 		/*
 		 * return Collections.singletonList( new CatalogItem("Transformers", "Test", 4)
 		 * );
 		 */
-		
-		
+
 	}
 
 }
