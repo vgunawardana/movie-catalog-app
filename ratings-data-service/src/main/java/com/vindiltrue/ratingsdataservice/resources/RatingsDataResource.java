9c202a06bc3aea817fa39ca0a3e22b5b43ec5984
vgunawardana <vindi121@gmail.com>
2019-10-15 16:01:13 +0530

    Call Ratings API using userID - missed files

--- ratings-data-service/src/main/java/com/vindiltrue/ratingsdataservice/resources/RatingsDataResource.java
+++ ratings-data-service/src/main/java/com/vindiltrue/ratingsdataservice/resources/RatingsDataResource.java
@@ -1,5 +1,8 @@
 package com.vindiltrue.ratingsdataservice.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,17 @@ public class RatingsDataResource {
 	public Rating getRatingsData(@PathVariable("movieId") String movieId) {
 		
 		return new Rating(movieId,4);
+	}
+	
+	@RequestMapping("users/{userId}")
+	public List<Rating> getUserRating(@PathVariable("userId") String userId) {
+		List<Rating> ratings = Arrays.asList(
+				new Rating("0001",5),
+				new Rating("0002", 4)
+				);
+		
+		
+		return ratings;
 	}
 
 }
